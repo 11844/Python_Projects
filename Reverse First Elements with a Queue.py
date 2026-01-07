cff59038a04049e03e4c6a682800543d4ecc390c
Bhuvan Gundu <bhuvangunduhyd@gmail.com>
2026-01-07 14:16:27 +0530

    modified:   Reverse First Elements with a Queue.py

--- Reverse First Elements with a Queue.py
+++ Reverse First Elements with a Queue.py
@@ -1,12 +1,17 @@
 from collections import deque
 
+
 def reverse_first_k(queue, k):
+
     stack = []
+
     for _ in range(k):
         stack.append(queue.popleft())
 
+
     while stack:
         queue.append(stack.pop())
+
 
     for _ in range(len(queue) - k):
         queue.append(queue.popleft())
@@ -14,7 +19,12 @@ def reverse_first_k(queue, k):
     return queue
 
 
+
 # ▶ Example Usage
-q = deque([1, 2, 3, 4, 5])
+jls_extract_var = 5
+q = deque([1, 2, 3, 4, jls_extract_var])
+
 k = 3
+
 print(list(reverse_first_k(q, k)))
+
